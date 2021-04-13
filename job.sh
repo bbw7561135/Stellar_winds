fcce12f121bb9dfe083aad87a0eeead0dfce228b
jomen93 <jsmendezh@unal.edu.co>
2021-04-13 17:46:04 -0500

    Modification to launch all files

--- job.sh
+++ job.sh
@@ -1,8 +1,11 @@
+echo "Cleaning all past files"
 make cleanall
 make
 if [ $? -eq 0 ];
   then
-  echo "Launch to server"
   qsub launch.pbs
-  make extract
+  if [ $? -eq 0 ];
+  	then
+  	echo "Launch to server"
+  fi
 fi
